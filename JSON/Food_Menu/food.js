b2be9d82a68b077e975428812d1d5d048d817b77
Disha Sathavara <dishasathavara.com>
2021-06-26 11:51:23 +0530

    Food menu using JSON

--- JSON/Food_Menu/food.js
+++ JSON/Food_Menu/food.js
@@ -62,34 +62,12 @@ function menuList() {
             // MenuData['menu'][i]='none';
             const li = document.createElement('li');
 
-            // let butonli=document.createElement('button');
-            // butonli.innerHTML='order';
-            // var body = document.getElementsByTagName("body")[0];
-            //         body.appendChild(butonli);
-
-            // li.setAttribute('id','Fli');
-            // console.log(li);
-            //    console.log([i][0]);
-            // var oli=document.getElementsByTagName('li');
-            // oli.appendChild(butonli);
-
             li.appendChild(ul);
-
-
-            // butonli.appendChild(li);
-            // console.log(li);
-            // console.log(MenuData);
-
-
-
-            // window.onload = function(){
-            //     document.getElementById("Fli").onclick=function(){return false};
-            // }
 
             li.innerHTML = i;
             // console.log(i);
             // console.log(li);
-            // li.style.pointerEvents='none';
+           
             ul.appendChild(li);
 
 
@@ -100,7 +78,7 @@ function menuList() {
             for (var j in MenuData['menu'][i]) {
                 if (typeof MenuData['menu'][i][j] == "object") {
                     const li = document.createElement('li');
-                    // document.getElementsByTagName('li');
+                    
 
                     li.innerHTML = j;
 
@@ -137,11 +115,6 @@ function menuList() {
             var button = document.createElement("button");
             button.innerHTML = "Add Order";
 
-
-            // // 2. Append somewhere
-            // var add = document.getElementsByTagName("li")[0];
-            // add.appendChild(button);
-            // console.log(li);
             li.innerHTML = i;
             ul.appendChild(li);
         }
@@ -173,10 +146,9 @@ btn.addEventListener('click', () => {
         document.getElementById('showUl').innerHTML = "";
         document.getElementById('showUl').style.pointerEvents = 'none';
         document.getElementById('showUl').style.pointerEvents = null;
-        // document.querySelectorAll('#btn').hide = true;
+        
     }
-    // const li = document.createElement('li');
-    // li.setAttribute('id', 'push');
+    
     const orderList = JSON.parse(localStorage.getItem('order'));
     
 
@@ -187,7 +159,7 @@ btn.addEventListener('click', () => {
     else {
       
         JSON.stringify(localStorage.getItem('order'));
-        // document.querySelectorAll('#btn').hide;
+        
         orderList.map((order) => {
             const li = document.createElement('li');
            
@@ -207,7 +179,7 @@ btn.addEventListener('click', () => {
 })
 
 
-
+//Add item buttons 
 var li = document.querySelectorAll('#push');
 li.forEach(function(item){ 
     var button=document.createElement('button');
@@ -221,7 +193,7 @@ item.appendChild(button);
 
 var orders = [];
 
-// const li = document.createElement('li');
+
 
 [...document.querySelectorAll('#btn')].forEach(el => el.addEventListener('click', function (event) {
     
@@ -231,18 +203,11 @@ var orders = [];
 
     if (document.querySelectorAll('#btn')) {
         
-            // document.getElementsByTagName('button').hide;
-      
-        // document.getElementById('push');
-        // document.querySelectorAll('#push');
-        orders.push(event.target.parentNode.innerHTML);
+           
+        orders.push(event.target.parentNode.childNodes[0].nodeValue);
 
         localStorage.setItem('order', JSON.stringify(orders));
         console.log(event.target);
-
-
-        //    removeEventListener('click',function(event){
-        //     document.getElementById('orderlist');
 
         return orders;
 
